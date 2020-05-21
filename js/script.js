b1678e76ded51e105985f3945c987e43d650478d
luucasallan <appslucasallan@gmail.com>
2020-05-21 19:17:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // @ts-check
 // para executar só quando a página terminar de carregar
 window.addEventListener('load', start);
-
 //variaveis global
 var red = null;
 /**
@@ -32,7 +31,10 @@ var rgb = null;
  * @type {HTMLInputElement}
  */
 var box = null;
-
+/**
+ * @type {HTMLInputElement}
+ */
+var main = null;
 
 //para startar a aplicação
 function start() {
@@ -46,30 +48,25 @@ function start() {
   onBlue = document.querySelector('#onBlue');
   // div
   box = document.querySelector('#caixaRGB');
-  
-  //-----------------------------------------//
-
+  main = document.querySelector('#main');
   activateRenge();
   preventFormSubmit();
 }
-
 function preventFormSubmit() {
-   /**
-   * 
-   * @param {Event & {target: HTMLInputElement}} e 
-   */
+  /**
+  * 
+  * @param {Event & {target: HTMLInputElement}} e 
+  */
   function handleFormSubmit(e) {
     e.preventDefault();
   }
   var form = document.querySelector('form');
   form.addEventListener('submit', handleFormSubmit);
 }
-
 function activateRenge() {
   red.addEventListener('input', inRed);
   green.addEventListener('input', inGreen);
   blue.addEventListener('input', inBlue);
-
   var inR = '0';
   /**
    * 
@@ -80,19 +77,16 @@ function activateRenge() {
     onRed.value = inR;
     rgb();
   }
-
   var inG = '0';
-   /**
-   * 
-   * @param {Event & {target: HTMLInputElement}} e 
-   */
+  /**
+  * 
+  * @param {Event & {target: HTMLInputElement}} e 
+  */
   function inGreen(e) {
     inG = e.target.value;
     onGreen.value = inG;
     rgb();
   }
-  
-
   var inB = '0';
   /**
    * 
@@ -103,15 +97,17 @@ function activateRenge() {
     onBlue.value = inB;
     rgb();
   }
-
   function rgb() {
-
-    var color = inR + ',' + inG +','+ inB;
-    
+    var color = inR + ',' + inG + ',' + inB;
     box.style.background = "rgb(" + color + ")";
+
+    var inverted = `${~Number.parseInt(inR) & 255},
+                    ${~Number.parseInt(inG) & 255},
+                    ${~Number.parseInt(inB) & 255}`
+    main.style.background = "rgb(" + inverted  + ")";
+
 
   }
   rgb();
-
 }
 
